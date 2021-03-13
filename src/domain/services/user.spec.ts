ff4fc3573f1e8cdf11ea2fdd576bf971bc226f64
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2021-03-13 14:18:54 -0300

    test: ensure validate token return user data

--- src/domain/services/user.spec.ts
+++ src/domain/services/user.spec.ts
@@ -31,8 +31,8 @@ class UserRepoSut implements UserRepo {
   async get(userInfo: LoginUserDto): Promise<UserDto> {
     return await new Promise(resolve => resolve({
       id: 1,
-      name: 'someone',
-      email: 'someone@email.com',
+      name: userDto.name,
+      email: userDto.email,
       password: 'hashed_password',
     }));
   }
@@ -54,7 +54,7 @@ class JwtSut implements Jwt {
   }
 
   async decode(token: Token): Promise<any> {
-    return await new Promise((resolve) => resolve(null));
+    return await new Promise((resolve) => resolve(userDto));
   }
 }
 
@@ -113,5 +113,72 @@ describe('User Service add', () => {
     jest.spyOn(jwtSut, 'encode').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
     const promise = sut.add(userDto);
     await expect(promise).rejects.toThrow();
+  });
+});
+
+describe('User Service add', () => {
+  test('Should call encrypter with right password', async() => {
+    const { sut, encrypterSut } = makeSut();
+    const encrypterSpy = jest.spyOn(encrypterSut, 'isValid');
+    await sut.login(userDto);
+    expect(encrypterSpy).toBeCalledWith(userDto.password, 'hashed_password');
+  });
+
+  test('Should call repo with right data', async() => {
+    const { sut, repoSut } = makeSut();
+    const repoSpy = jest.spyOn(repoSut, 'get');
+    await sut.login(userDto);
+    expect(repoSpy).toBeCalledWith({ email: userDto.email, name: '', id: '', password: '' });
+  });
+
+  test('Should call jwt with right data', async() => {
+    const { sut, jwtSut } = makeSut();
+    const jwtSpy = jest.spyOn(jwtSut, 'encode');
+    await sut.login(userDto);
+    expect(jwtSpy).toBeCalledWith({ id: 1, name: userDto.name, email: userDto.email });
+  });
+
+  test('Should return token if all data is right', async() => {
+    const { sut } = makeSut();
+    const token = await sut.login(userDto);
+    expect(token).toEqual({ token: 'valid_token' });
+  });
+
+  test('Should throw if encrypter throws', async() => {
+    const { sut, encrypterSut } = makeSut();
+    jest.spyOn(encrypterSut, 'isValid').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+    const promise = sut.login(userDto);
+    await expect(promise).rejects.toThrow();
+  });
+
+  test('Should throw if repo throws', async() => {
+    const { sut, repoSut } = makeSut();
+    jest.spyOn(repoSut, 'get').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+    const promise = sut.login(userDto);
+    await expect(promise).rejects.toThrow();
+  });
+
+  test('Should throw if jwt throws', async() => {
+    const { sut, jwtSut } = makeSut();
+    jest.spyOn(jwtSut, 'encode').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+    const promise = sut.login(userDto);
+    await expect(promise).rejects.toThrow();
+  });
+});
+
+describe('User Service validateToken', () => {
+  const token: Token = { token: 'valid_token' };
+
+  test('Should throw if jwtSut throws', async() => {
+    const { sut, jwtSut } = makeSut();
+    jest.spyOn(jwtSut, 'decode').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+    const promise = sut.validateToken(token);
+    await expect(promise).rejects.toThrow();
+  });
+
+  test('Should return user data', async() => {
+    const { sut } = makeSut();
+    const result = await sut.validateToken(token);
+    expect(result).toEqual(userDto);
   });
 });
