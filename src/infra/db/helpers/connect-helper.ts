8a1f379f765465d54b4cd953b369c3c8e10149cb
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2021-03-13 01:53:39 -0300

    refactor: set connect-helper to accept ssl

--- src/infra/db/helpers/connect-helper.ts
+++ src/infra/db/helpers/connect-helper.ts
@@ -3,7 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 pool.on('error', (err, client) => {
   console.log('Postgres Error', err);
