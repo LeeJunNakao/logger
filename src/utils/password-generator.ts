b208834f675404f070476a2a554cf40d145d9c03
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2021-05-02 13:12:42 -0300

    refactor: modify password generator algorith to create strong passwords

--- src/utils/password-generator.ts
+++ src/utils/password-generator.ts
@@ -8,12 +8,38 @@ export class PasswordGenerator implements IPasswordGenerator {
     return characters[index];
   }
 
+  private generateNumber(): string {
+    const number = Math.floor(Math.random() * 9);
+    return String(number);
+  }
+
+  private generateSpecialCharacter(): string {
+    const characters = String.raw`^[-#!$%^&*()_+|~=\`{}\[\]:";'<>?,.\/ ]$`;
+    const index = Math.floor(Math.random() * (characters.length - 1));
+    return characters[index];
+  }
+
+  private generateLetter(upper?: boolean): string {
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    const index = Math.floor(Math.random() * (characters.length - 1));
+    const char = characters[index];
+    return upper ? char.toUpperCase() : char;
+  }
+
   generate(len: number = 7): string {
     const password: string[] = [];
-    for (let i = 0; i < len; i++) {
+    if (len < 4) throw new Error();
+
+    password.push(this.generateLetter());
+    password.push(this.generateLetter(true));
+    password.push(this.generateSpecialCharacter());
+    password.push(this.generateNumber());
+
+    for (let i = 4; i < len; i++) {
       const char = this.generateChar();
       password.push(char);
     }
+
     return password.join('');
   }
 }
